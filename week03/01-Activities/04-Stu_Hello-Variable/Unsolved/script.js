43a8c80bc32b0f40a2333418aa3322583e1b4589
Keziah Stewart <kezie@Keziahs-MBP.lan>
2022-07-25 21:57:25 -0600

    added week3

---
+++ week03/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,8 @@
+// Write Your JavaScript Code Here
+var studentName = " Kezie ";
+var pets = 4;
+var funFact = 'The gender of reptiles is dependant of the temperature in which the nest is buried';
+
+console.log (" My name is " + studentName);
+console.log (" I have " + pets + " pets ");
+console.log (" A fun fact that I know " + funFact);
---
+++ week03/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,23 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof one); Number
+console.log(typeof two); String
+console.log(typeof three); Boolean
+console.log(typeof four); Number
+console.log(typeof five); String
+console.log(typeof six); undefined
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof four); String
+console.log(typeof five); Boolean
+console.log(typeof six); Number
